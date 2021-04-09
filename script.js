e01142e56b9c2cf5eff1cdcf7e9e6ad45cd36765
Rifqi Fadhlurrakhman <43606903+rifqifdr@users.noreply.github.com>
2021-04-09 18:29:26 +0700

    testing sw

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register(".serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
 var app = new Vue({
   el: "#app",
   data: {
---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+const staticDevCoffee = "the-war-on-everyone"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
